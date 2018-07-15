1bde9c7042badfe95ea2ecba307a181b48d1f803
Hea3veN <hea3venmc@gmail.com>
2018-07-15 10:33:16 -0300

    * Fix instagram news source

--- src/main/kotlin/com/hea3ven/gfriendmedianews/mods/medianews/source/InstagramNewsSource.kt
+++ src/main/kotlin/com/hea3ven/gfriendmedianews/mods/medianews/source/InstagramNewsSource.kt
@@ -12,6 +12,7 @@ import java.io.InputStreamReader
 import java.net.URL
 import java.text.ParseException
 import java.util.*
+import java.util.regex.Pattern
 
 class InstagramNewsSource : NewsSource() {
 	private val logger = LoggerFactory.getLogger(
@@ -32,27 +33,49 @@ class InstagramNewsSource : NewsSource() {
 			lastDate = Date(0)
 		}
 		try {
-			URL("https://www.instagram.com/" + sourceConfig.connectionData + "/?__a=1").openStream().use { stream ->
+			URL("https://www.instagram.com/" + sourceConfig.connectionData).openStream().use { stream ->
 				val posts = try {
-					JsonParser().parse(InputStreamReader(stream)).asJsonObject
+					val pageText = InputStreamReader(stream).readText()
+					val m = Pattern.compile("window\\._sharedData = ([^;]*);").matcher(pageText)
+					m.find()
+					JsonParser().parse(m.group(1)).asJsonObject
 				} catch (e: Exception) {
 					logger.warn("Could not read from instagram")
 					logger.trace("Error connecting to instagram", e)
 					return listOf()
 				}
-				val result = posts.getAsJsonArray("items")
+				val user = posts.getAsJsonObject("entry_data")
+						.getAsJsonArray("ProfilePage")[0]
+						.asJsonObject.getAsJsonObject("graphql")
+						.getAsJsonObject("user")
+				val userName = "@" + user.get("username").asString
+				val userUrl = "https://www.instagram.com/" + user.get("username").asString
+				val userIcon = user.get("profile_pic_url_hd").asString
+				val result = user
+						.getAsJsonObject("edge_owner_to_timeline_media")
+						.getAsJsonArray("edges")
+						.map { it.asJsonObject.getAsJsonObject("node") }
 						.map { post ->
 							val caption = post.asJsonObject.getAsJsonObject("caption")
-							val img = post.asJsonObject.getAsJsonObject("images")
+//							val img = post.asJsonObject.getAsJsonObject("images")
 
-							val date = Date(caption.get("created_time").asLong * 1000)
-							val userName = "@" + caption.getAsJsonObject("from").get("username").asString
-							val userUrl = "https://www.instagram.com/" + caption.getAsJsonObject("from").get(
-									"username").asString
-							val userIcon = caption.getAsJsonObject("from").get("profile_picture").asString
-							val url = post.asJsonObject.get("link").asString
-							val picUrl = img.getAsJsonObject("standard_resolution").get("url").asString
-							val text = escapeLinks(caption.get("text").asString)
+//							val date = Date(caption.get("created_time").asLong * 1000)
+							val date = Date(post.get("taken_at_timestamp").asLong * 1000)
+
+//							val userName = "@" + caption.getAsJsonObject("from").get("username").asString
+//							val userUrl = "https://www.instagram.com/" + caption.getAsJsonObject("from").get(
+//									"username").asString
+//							val userIcon = caption.getAsJsonObject("from").get("profile_picture").asString
+
+//							val url = post.asJsonObject.get("link").asString
+							val url = "https://www.instagram.com/p/" + post.asJsonObject.get("shortcode").asString
+//							val picUrl = img.getAsJsonObject("standard_resolution").get("url").asString
+							val picUrl = post.get("display_url").asString
+//							val text = escapeLinks(caption.get("text").asString)
+							val text = post.getAsJsonObject("edge_media_to_caption")
+									.getAsJsonArray("edges")[0]
+									.asJsonObject.getAsJsonObject("node")
+									.get("text").asString
 							NewsPost(Color(205, 72, 107), date, userName, userUrl, userIcon, this, url,
 									text, listOf(picUrl))
 						}
@@ -62,7 +85,7 @@ class InstagramNewsSource : NewsSource() {
 					sourceConfig.stateData = Config.dateFmt.format(result.last().date)
 				return result
 			}
-		} catch(e: IOException) {
+		} catch (e: IOException) {
 
 			logger.warn("Could not read from instagram")
 			logger.trace("Error connecting to instagram", e)
