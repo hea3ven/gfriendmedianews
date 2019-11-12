e501f7d3728361005ba719ff48251fd7fb431a7d
Matias Ribecky <matias.ribecky@mercadolibre.com>
2019-11-12 14:57:52 -0300

    Add prometheus metrics exporter

--- src/main/kotlin/com/hea3ven/gfriendmedianews/main.kt
+++ src/main/kotlin/com/hea3ven/gfriendmedianews/main.kt
@@ -2,20 +2,29 @@ package com.hea3ven.gfriendmedianews
 
 import com.hea3ven.gfriendmedianews.persistance.Persistence
 import de.btobastian.javacord.Javacord
+import io.prometheus.client.exporter.HTTPServer
+import io.prometheus.client.hotspot.DefaultExports
 import org.slf4j.LoggerFactory
 
 private val logger = LoggerFactory.getLogger("com.hea3ven.gfriendmedianews.main")
 fun main(args: Array<String>) {
     try {
+        val prometheusServer = initPrometheus()
         Persistence().use { persistence ->
             val api = Javacord.getApi(Config.discordApiToken, true)
             val mediaNews = ChinguBot(persistence, api)
             mediaNews.start()
         }
+        prometheusServer.stop()
     } catch (e: Throwable) {
         logger.error("Unhandled exception", e)
     } finally {
         System.exit(0)
     }
+}
+
+fun initPrometheus(): HTTPServer {
+    DefaultExports.initialize()
+    return HTTPServer(8080, true)
 }
 
